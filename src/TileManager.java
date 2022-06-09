49bb0c2c5692b28d22f962cb65e84e668f3864e6
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2022-06-09 14:15:22 -0400

    maps

--- src/TileManager.java
+++ src/TileManager.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
 
 public class TileManager {
 
     GamePanel gp;
     Tile[] tile;
+    int mapTileNum[][];
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
@@ -18,11 +20,18 @@ public class TileManager {
     public void getTileImage() {
         try {
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/res/max/max_left.png"));
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/background/tile1.png"));
+
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/background/tile2.png"));
 
         } catch (IOException e) {
             System.out.println("IOExceptio " + e);
         }
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
     }
 
 }
