e40bcf3dbe2116bdc200f7a388ab8d525d86c00f
yasaidaisuki <93557123+yasaidaisuki@users.noreply.github.com>
2022-06-15 00:32:50 -0400

    hmmm
    h

--- src/Tile.java
+++ src/Tile.java
@@ -1,8 +1,19 @@
 import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+import java.awt.Image;
 
 public class Tile {
 
-    public BufferedImage image;
+    public Image image;
     public boolean collision = false;
+    public Rectangle blockHitBox;
+    public int num;
+
+    public Tile(Image image, Rectangle blockHitBox, boolean collision, int num) {
+        this.image = image;
+        this.blockHitBox = blockHitBox;
+        this.collision = collision;
+        this.num = num;
+    }
 
 }
