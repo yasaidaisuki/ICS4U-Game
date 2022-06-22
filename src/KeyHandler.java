84184d4b70d3900e67bf6b4e99358f85d756dd00
yasaidaisuki <93557123+yasaidaisuki@users.noreply.github.com>
2022-06-21 21:32:56 -0400

    title screen
    menu screen commands

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -4,10 +4,20 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
+	// for menu controls
+	GamePanel gp;
 	// for attack
 	boolean flag = false;
 	boolean jump, left, right, attack; // check for player input
 	long startAtk = System.currentTimeMillis();
+
+	// Name: KeyHandler
+	// Purpose: constructor
+	// Param:
+	// Return:
+	public KeyHandler(GamePanel gp) {
+		this.gp = gp;
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -21,28 +31,52 @@ public class KeyHandler implements KeyListener {
 	// Return: void
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_A) {
-			left = true;
-			right = false;
-		} else if (key == KeyEvent.VK_D) {
-			right = true;
-			left = false;
-		} else if (key == KeyEvent.VK_W) {
-			jump = true;
-		} else if (key == KeyEvent.VK_J) {
-			long currentTime = System.currentTimeMillis();
-			if (attack == false && currentTime - startAtk >= 800 && flag == false) {
-				attack = true;
-				flag = true;
-				jump = false;
-				right = false;
-				left = false;
-				startAtk = System.currentTimeMillis();
-			}
-			else {
-				attack = false;
-			}
 
+		// title state
+		if (gp.gameState == gp.titleState) {
+			// scrolling down commands
+			if (key == KeyEvent.VK_W) {
+				gp.commandNum ++;
+			}
+			// resets the scroll if it reaches the end
+			if (gp.commandNum >= 5) {
+				gp.commandNum = 0;
+			}
+			// enter command
+			if (key == KeyEvent.VK_ENTER) {
+				if(gp.commandNum == 0) {
+					gp.gameState = gp.playState;
+				}
+				if (gp.commandNum == 4) {
+					System.exit(0);
+				}
+			}
+		}
+		
+		// game play state
+		else if (gp.gameState == gp.playState) {
+			if (key == KeyEvent.VK_A) {
+				left = true;
+				right = false;
+			} else if (key == KeyEvent.VK_D) {
+				right = true;
+				left = false;
+			} else if (key == KeyEvent.VK_W) {
+				jump = true;
+			} else if (key == KeyEvent.VK_J) {
+				long currentTime = System.currentTimeMillis();
+				if (attack == false && currentTime - startAtk >= 800 && flag == false) {
+					attack = true;
+					flag = true;
+					jump = false;
+					right = false;
+					left = false;
+					startAtk = System.currentTimeMillis();
+				} else {
+					attack = false;
+				}
+
+			}
 		}
 	}
 
